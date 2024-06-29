92a533cf952d748f8c98753a15415c389e73b386
avenegas91 <aven2601@yahoo.com>
2024-06-28 22:30:03 -0400

    Changed authController to throw error if username is already taken

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,10 +7,20 @@ const register = async (req, res) => {
   const { username, password, birthdate } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
+    console.log('Register request:', { username, password, birthdate });
+
+    // Check if the username already exists
+    const existingUser = await User.findByUsername(username);
+    console.log('Existing user:', existingUser);
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
     const userId = await User.create(username, hashedPassword, birthdate);
     res.json({ userId });
   } catch (error) {
-    console.error('Registration error:', error);
+    console.error('Registration error:', error.message || error);
     res.status(500).json({ error: 'User registration failed' });
   }
 };
@@ -27,7 +37,7 @@ const login = async (req, res) => {
     const token = jwt.sign({ userId: user.id }, secretKey);
     res.json({ token });
   } catch (error) {
-    console.error('Login error:', error);
+    console.error('Login error:', error.message || error);
     res.status(500).json({ error: 'Login failed' });
   }
 };
